42a8a0d651bf6ed9f9992e81b204a1a49710c350
DioSyahrizal <diosyahrizal@gmail.com>
2021-07-14 14:14:19 +0700

    feat: initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const next = require("next");
+const express = require("express");
+
+const dev = process.env.NODE_ENV !== "production";
+const port = process.env.PORT || 3000;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Listening to port ${port}`);
+  });
+});
